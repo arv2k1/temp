7b3a2a552d9cb0680d34d233c430273cd28b20ba
arv2k1 <76536617+arv2k1@users.noreply.github.com>
2021-12-03 18:14:15 +0530

    Update wget-miner.sh

--- wget-miner.sh
+++ wget-miner.sh
@@ -9,3 +9,6 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux
 tar -xf xmrig-6.16.2-linux-static-x64.tar.gz
 
 # Replace the config file with your custom config file with details about wallet address and mining pool url and password
+cd xmrig-6.16.2
+rm config.json
+wget https://raw.githubusercontent.com/arv2k1/temp/main/config.json
